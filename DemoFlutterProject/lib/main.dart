1b047bc9f4776fd7b562f9646f1d51922f40fcf1
VinayTShetty <vinaytshetty@gmail.com>
2022-11-11 13:23:36 +0530

    Added AppBar Code to the Project. With Center Text FabButton Click.

--- DemoFlutterProject/lib/main.dart
+++ DemoFlutterProject/lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Demo Project'),
-      ),
-    ),
-  ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Demo Project'),
+            centerTitle: true,
+          ),
+          body: Center(
+              child: Text('Hello Project')
+          ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Press'),
+          onPressed: () { 
+            print('Fab Clicked');
+          },
+        ),
+      )
+  )
+  );
 }
