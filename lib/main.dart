fb8c825d6e80e3cbb1f801430871b8996831f1e8
VinayTShetty <vinaytshetty@gmail.com>
2024-01-11 16:41:10 +0530

    BLOC Listner Implementation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -57,6 +58,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          BlocListener<CounterBloc, CounterState>(
+            listenWhen: (previous, current) => true,
+            listener: (context, state) {
+              if (state.counter >= 5) {
+                final snackBar = SnackBar(
+                  /// need to set following properties for best effect of awesome_snackbar_content
+                  elevation: 0,
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.transparent,
+                  content: AwesomeSnackbarContent(
+                    title: 'On Snap!',
+                    message:
+                        'This is an example error message that will be shown in the body of snackbar!',
+
+                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                    contentType: ContentType.success,
+                  ),
+                );
+
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
+              }
+            },
+            child: Text('Bloc Listner'),
+          ),
           BlocBuilder<CounterBloc, CounterState>(
               buildWhen: (previous, current) {
             /**
